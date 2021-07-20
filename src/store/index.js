361de707485e852ce31d811d09a61e5e0a30201a
RahimCml <rhmcml@mail.ru>
2021-07-21 00:13:38 +0400

    Send get request get data and print data in Home.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://www.omdbapi.com'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    movie: []
   },
-  mutations: {
-  },
+  mutations: {},
   actions: {
-  },
-  modules: {
+    async fetchMovie () {
+      const req = await axios.get('?i=tt0337898&apikey=e9e9888e')
+      return req.data
+    }
   }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import { mapActions } from 'vuex'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      title: []
+    }
+  },
+  async created () {
+    this.title = await this.fetchMovie
+  },
+  methods: {
+    ...mapActions(['fetchMovie'])
   }
 }
 </script>
+
+<template lang="pug">
+  .home
+    h1 Movie {{title.length}}
+    ol
+      li name: {{title || 'No detail'}}
+</template>
