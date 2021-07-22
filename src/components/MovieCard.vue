ff25ca666b3be64f992e06b68d06a7e8cb979fa5
RahimCml <rhmcml@mail.ru>
2021-07-23 01:01:43 +0400

    Map action and state

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,24 @@
+<script>
+
+import { mapActions, mapState } from 'vuex'
+
+export default {
+  name: 'MovieCard',
+  data () {
+    return {
+      selectedMoive: ''
+    }
+  },
+  methods: {
+    ...mapActions(['fetchMovie'])
+  },
+  computed: {
+    ...mapState(['movies'])
+  }
+}
+</script>
+
+<template>
+  <div class="movieCard">
+  </div>
+</template>
