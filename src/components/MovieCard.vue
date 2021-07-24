3ef92b3f74fc6504ca6367a819d41f43ea269797
RahimCml <rhmcml@mail.ru>
2021-07-24 12:12:43 +0400

    Assign movieCard content to home Keep props named data in MovieCard

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,36 +1,15 @@
 <script>
 
-import { mapActions, mapState } from 'vuex'
-
 export default {
   name: 'MovieCard',
-  data () {
-    return {
-      selectedMoive: ''
-    }
-  },
-  async mounted () {
-    await this.fetchMovie()
-  },
-  methods: {
-    ...mapActions(['fetchMovie'])
-  },
-  computed: {
-    ...mapState(['movies'])
+  props: {
+    data: String
   }
 }
 </script>
 
 <template>
   <div class="movieCard">
-    <div class="searchMovie">
-      <input type="text" placeholder="Film secimi...">
-      <button>Search</button>
-    </div>
-    <ul>
-      <li v-for="movie in movies" :key="movie.Title">
-        {{ movie.Title }}
-      </li>
-    </ul>
+    <p>{{ data }}</p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,37 @@
 <script>
 
+import { mapActions, mapState } from 'vuex'
 import MovieCard from '@/components/MovieCard.vue'
 
 export default {
   name: 'Home',
   components: {
     MovieCard
+  },
+  async mounted () {
+    console.log(this.data)
+    await this.fetchMovie()
+  },
+  methods: {
+    ...mapActions(['fetchMovie'])
+  },
+  computed: {
+    ...mapState(['movies'])
   }
 }
 </script>
 
 <template>
   <div class="home">
-    <MovieCard />
+    <div class="searchMovie">
+      <input type="text" placeholder="Film secimi...">
+      <button>Search</button>
+    </div>
+    <ul>
+      <li v-for="movie in movies" :key="movie.Title">
+        <MovieCard data="movie"/>
+        <p>{{ movie.Title }} year {{movie.Year}}</p>
+      </li>
+    </ul>
   </div>
 </template>
