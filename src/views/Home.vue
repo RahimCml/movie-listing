b7b89b44ed7e0fd9b963c66489981f572430ada6
RahimCml <rhmcml@mail.ru>
2021-07-30 03:04:18 +0400

    Use if else and print when movie is not found

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -34,10 +34,13 @@ export default {
       <input type="text" v-model="search" placeholder="Film seciniz...">
       <button @click="getMoviesWithSearch">Search</button>
     </div>
-    <div class="container">
+    <div class="container" v-if="movies">
       <div v-for="movie in movies" :key="movie.imdbId">
         <MovieCard :data="movie"/>
       </div>
+    </div>
+    <div class="noFind" v-else>
+      <h1>Movie is not found</h1>
     </div>
   </div>
 </template>
