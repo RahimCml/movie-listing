8ce3ed2df43f6d84afeb26e5ce389d7a718cf134
RahimCml <rhmcml@mail.ru>
2021-08-01 04:07:06 +0400

    Make state and action maps

---
+++ src/views/Movie.vue
@@ -0,0 +1,22 @@
+<script>
+
+import { mapActions, mapState } from 'vuex'
+
+export default {
+  name: 'MovieAbout',
+  methods: {
+    ...mapActions(['fetchMovieByImdbID'])
+  },
+  computed: {
+    ...mapState(['movie'])
+  }
+}
+</script>
+
+<template>
+   <div class="home">
+   </div>
+</template>
+
+<style scoped>
+</style>
